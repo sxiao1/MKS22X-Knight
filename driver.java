5ca6256a8890f8eda65897d281e25fde030bac15
sxiao1 <sxiao1@stuy.edu>
2019-02-22 12:31:11 -0500

    updated driver

--- driver.java
+++ driver.java
@@ -1,6 +1,6 @@
 public class driver{
   public static void main(String[] args){
-    KnightBoard newboard = new KnightBoard(5,5);
+    KnightBoard newboard = new KnightBoard(3,4);
     System.out.println(newboard.solve(0,0));
     System.out.println(newboard);
   }
