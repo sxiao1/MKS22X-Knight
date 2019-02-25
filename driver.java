6c78bc39855430c2e5b23eb8de47c5a73f573111
sxiao1 <sxiao1@stuy.edu>
2019-02-25 10:14:56 -0500

    tested test cases and failed and will work on it later today

--- driver.java
+++ driver.java
@@ -1,9 +1,10 @@
 public class driver{
   public static void main(String[] args){
-    KnightBoard newboard = new KnightBoard(3,4);
+    /*KnightBoard newboard = new KnightBoard(3,4);
     //System.out.println(newboard);
     System.out.println(newboard.countSolutions(1,0));
     //System.out.println(newboard.solve(0,0));
-    //System.out.println(newboard);
+    //System.out.println(newboard); */
+    System.out.println(KnightBoard.checker(1));
   }
 }
